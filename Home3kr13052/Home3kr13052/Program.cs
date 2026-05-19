3b1f1e38f633591f26e4a64c6568ae5aae31b73e
Жасмина Иманбаева <nanamioku11@gmail.com>
2026-05-19 22:25:27 +0700

    РАБОТА С ТЕКСТОВЫМИ ФАЙЛАМИ И СПИСКАМИ

---
+++ Home3kr13052/Home3kr13052/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+namespace Home3kr13052
+{
+    internal class Programm
+    {
+        static bool Solution9(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+            var set1 = new Dictionary<char, int>();
+            foreach(char c in s)
+                set1[c] = set1.GetValueOrDefault(c)+1;
+            foreach (char c in t)
+            {
+                if (!set1.ContainsKey(c)) return false;
+                if (set1[c] < 0) return false;
+            }
+            return true;
+        }
+        static int[] Solution10(string[] queries, string[] words)
+        {
+            int[] wunique = new int[words.Length];
+            for(int i = 0; i < words.Length; i++)
+            {
+                wunique[i] = new HashSet<char>(words[i]).Count;
+            }
+            Array.Sort(wunique);
+            int[] res = new int[queries.Length];
+            for(int i = 0; i < queries.Length; i++)
+            {
+                int qunique = new HashSet<char>(queries[i]).Count;
+                int count = 0;
+                foreach (int w in wunique)
+                {
+                    if (w > qunique) count++;
+                }
+                res[i] = count;
+            }
+            return res;
+        }
+        static int Solution11(int[] nums)
+        {
+            var set = new Dictionary<int, int>();
+            foreach (int i in nums)
+            {
+                if (set.ContainsKey(i))
+                    return i;
+                else set[i] = 1;
+            }
+            return -1;
+        }
+        static char Solution12(string s, string t)
+        {
+            var set = new Dictionary<char, int>();
+            foreach(char c in t)
+                set[c] = set.GetValueOrDefault(c)+1;
+            foreach (char c in s)
+                set[c] = set.GetValueOrDefault(c) + -1;
+            foreach (var pair in set)
+            {
+                if (pair.Value > 0) return pair.Key;
+            }
+            return '-';
+        }
+        static void Main()
+        {
+            //Console.WriteLine(Solution9("listek", "silent")); 9 задача
+
+            //string[] res1 = { "aabb", "abcd", "a"};
+            //string[] res2 = { "abc", "ab", "aab", "aaaa" };
+            //int[] res = Solution10(res1,res2);
+            //foreach (int i in res) Console.WriteLine(i); 10 задача
+
+            //Console.WriteLine(Solution11([1,2,3,2,1])); 11 задача
+
+            //Console.WriteLine(Solution12("abd", "ff")); 12 задача
+        }
+    }
+}
---
+++ Kr31405/Kr31405/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace kr31405
+{
+    internal class Program //Иманбаева Жасмина 110б
+    {
+        static int Solution(int[] nums)
+        {
+            var set = new HashSet<int>(nums);
+            for(int i = 0; i <= nums.Length; i++)
+            {
+                if(!set.Contains(i)) return i;
+            }
+            return 0;
+        }
+        static void Main()
+        {
+            int[] nums = { 3, 0, 1 }; //2
+            int[] nums1 = { 0, 2, 4, 6, 8, 1, 3, 7, 9 }; //5
+            int[] nums2 = { 0,1,2,3,6,5,7,8,9,10,}; //4
+            Console.WriteLine("Ответ nums1: " + Solution(nums));
+            Console.WriteLine("Ответ nums2: " + Solution(nums1));
+            Console.WriteLine("Ответ nums3: " + Solution(nums2));
+        }
+    }
+}
